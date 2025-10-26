47c333b6180a586f57e1a502374913f0b0b3845e
randomSmarts <67401115+randomSmarts@users.noreply.github.com>
2025-10-26 11:15:27 -0700

    ee

--- backend/speech_to_text.py
+++ backend/speech_to_text.py
@@ -3,13 +3,15 @@ import queue
 import vosk #does offline speech recognition
 import json #parse's the recognition results from Vosk
 
-q = queue.Queue()
+q = queue.Queue() #creates a new queue
 
-def callback(indata, frames, time, status):
-    q.put(bytes(indata))
+def callback(indata, frames, time, status): #runs whenever the there is a chunk of sound to capture
+    #indata is the raw audio data, frames is the # of samples, time is the timestamp when it was recorded, status gives error data
+    q.put(bytes(indata)) #converts raw audio data into bytes and pushes into queue for main thread to process
 
 model = vosk.Model("/Users/aarshmittal/Downloads/vosk-model-en-us-0.22")  # Path to your model
-rec = vosk.KaldiRecognizer(model, 16000)
+rec = vosk.KaldiRecognizer(model, 16000) #sets up the recorder to process the bytes
+#kaldi is a right weight speech to text 
 
 with sd.RawInputStream(samplerate=16000, blocksize=8000, dtype='int16',
                        channels=1, callback=callback):
